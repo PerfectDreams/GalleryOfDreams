09e98674df6c1776f76262894a2bc89eabc5a574
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-03 18:59:46 -0300

    Remove :sparkles: inspirational text :sparkles: from the HomeOverview

--- frontend/src/jsMain/kotlin/net/perfectdreams/galleryofdreams/frontend/components/HomeOverview.kt
+++ frontend/src/jsMain/kotlin/net/perfectdreams/galleryofdreams/frontend/components/HomeOverview.kt
@@ -24,21 +24,6 @@ fun HomeOverview(
     data: GalleryOfDreamsDataWrapper,
     i18nContext: I18nContext
 ) {
-    // Sabia que existem pessoas que fazem artes maravilhosas de mim só porque eu ajudei no servidor delas? Eu até me derreto por dentro com tanta arte fofa de mim!
-    //
-    //Então para mostrar as incríveis artes que fizeram, eu resolvi colocar nesta página todas as fan arts que fizeram! Obrigada a todos que fizeram estas fan arts, eu fico feliz que existe tanta gente assim que gosta do que eu faço!
-    //
-    //Aliás, se você quiser enviar uma fan art para mim, então envie no meu servidor de suporte! Se a sua arte ficar legal ela poderá aparecer aqui!
-    //
-    //Para ver as fan arts, comece clicando para ver todas as fan arts que já fizeram ou clique em um artista para ver os que ela fez. Divirta-se apreciando as artes!
-
-    // Erisly:
-    // Thanks to all of the artists who've made art of me! I really appreciate it and really love all of your works! Clicking on an artwork will allow you to see the artist and any links they wished to provide. Submit fan art via the #fanart channel in Erisly's Official Discord Server to be added onto here!
-
-    // Pollux:
-    // Artwork made with love by our community members, users, and friends! Send your artworks using either this form or tweeting them to @maidPollux!
-    // This gallery contains artwork not only from Pollux but from her friends. Event mascots that seasonally appear from time to time~
-
     Div(
         attrs = {
             style {
