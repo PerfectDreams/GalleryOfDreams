386b35f4caaafc3448a749f1f4c12c1982a9ffb4
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-05 23:07:12 -0300

    Add missing index to the "FanArtTags" table

--- backend/src/main/kotlin/net/perfectdreams/galleryofdreams/backend/tables/FanArtTags.kt
+++ backend/src/main/kotlin/net/perfectdreams/galleryofdreams/backend/tables/FanArtTags.kt
@@ -5,6 +5,6 @@ import net.perfectdreams.galleryofdreams.common.FanArtTag
 import org.jetbrains.exposed.dao.id.LongIdTable
 
 object FanArtTags : LongIdTable() {
-    val fanArt = reference("fan_art", FanArts)
+    val fanArt = reference("fan_art", FanArts).index()
     val tag = postgresEnumeration<FanArtTag>("tag")
 }
