4a99b59760248a2227802a263399ca09d24fcb6b
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-17 21:26:22 -0300

    (Try to) correctly invalidate the SVG content by using DomSideEffect
    Using ref does not work, it keeps the icon content if the card changes by changing the category

--- frontend/src/jsMain/kotlin/net/perfectdreams/galleryofdreams/frontend/components/UIIcon.kt
+++ frontend/src/jsMain/kotlin/net/perfectdreams/galleryofdreams/frontend/components/UIIcon.kt
@@ -5,6 +5,7 @@ import net.perfectdreams.galleryofdreams.frontend.utils.IconManager
 import net.perfectdreams.galleryofdreams.frontend.utils.Svg
 import org.jetbrains.compose.web.attributes.AttrsBuilder
 import org.jetbrains.compose.web.dom.AttrBuilderContext
+import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Img
 import org.w3c.dom.Element
 import org.w3c.dom.HTMLImageElement
@@ -22,20 +23,19 @@ fun UIIcon(icon: IconManager.Icon, attrs: AttrBuilderContext<Element>? = null) {
         is IconManager.SVGIcon -> {
             Svg(
                 {
-                    ref { element ->
-                        val viewBox = icon.element.getAttribute("viewBox")
-                        if (viewBox != null)
-                            element.setAttributeNS(null, "viewBox", viewBox)
-
-                        icon.element.children.asList().forEach {
-                            element.appendChild(it.cloneNode(true))
-                        }
-                        onDispose {}
-                    }
-
                     (attrs as AttrBuilderContext<*>?)?.invoke(this as AttrsBuilder<Element>)
                 }
-            )
+            ) {
+                DomSideEffect(icon) { ref ->
+                    val viewBox = icon.element.getAttribute("viewBox")
+                    if (viewBox != null)
+                        ref.setAttributeNS(null, "viewBox", viewBox)
+
+                    icon.element.children.asList().forEach {
+                        ref.appendChild(it.cloneNode(true))
+                    }
+                }
+            }
         }
     }
 }
