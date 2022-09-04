6b91185bdffb6983d9f9ef8e8fcdbaef2debb0ed
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-09-04 18:28:12 -0300

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    const val GALLERY_OF_DREAMS = "1.0.9"
-    const val KOTLIN = "1.6.21"
+    const val GALLERY_OF_DREAMS = "1.0.10"
+    const val KOTLIN = "1.7.20"
     const val JIB = "3.2.1"
     const val KOTLINX_SERIALIZATION = "1.3.2"
     const val PROMETHEUS = "0.12.0"
-    const val KTOR = "2.0.0"
+    const val KTOR = "2.1.0"
     const val I18N_HELPER = "0.0.3-SNAPSHOT"
 }
